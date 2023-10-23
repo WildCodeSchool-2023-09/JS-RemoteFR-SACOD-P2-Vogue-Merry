7e0fafc99a337e8fbc30ad146d6ad9319622346e
Baris G <barisg@pm.me>
2023-10-23 19:04:23 +0200

    Layout basique fait + CSS fait (manque le responsive)

--- frontend/src/components/OngletElixir.jsx
+++ frontend/src/components/OngletElixir.jsx
@@ -1,20 +1,33 @@
+import searchIcon from "../assets/search.svg";
+
 function OngletElixir() {
   return (
     <div className="onglet-elixir pt-6 max-w-7xl flex flex-col m-auto">
-      <div className="barre-de-recherche-elixir">
-        <input type="text" name="" id="" />
+      <div className="barre-de-recherche-elixir flex items-center justify-center gap-8">
+        <img
+          src={searchIcon}
+          alt="search icon"
+          className="w-10 drop-shadow-md"
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="  Chercher un elixir"
+          className="rounded-xl shadow-xl input-searchbar transition p-1"
+        />
       </div>
-      <div className="filtre-elixir flex justify-around text-white p-6">
-        <p className="bg-purple-heart-500 px-10 rounded-lg hover:bg-purple-heart-800 py-1 shadow-md">
+      <div className="filtre-elixir flex justify-center gap-10 max-sm:gap-2 text-white p-6 font-bold max-sm:flex-col mb-10">
+        <p className="bg-purple-heart-500 px-24 text-center rounded-lg hover:bg-purple-heart-800 py-1 shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] transition">
           Tous
         </p>
-        <p className="bg-purple-heart-500 px-10 rounded-lg hover:bg-purple-heart-800 py-1 shadow-md">
+        <p className="bg-purple-heart-500 px-24 text-center rounded-lg hover:bg-purple-heart-800 py-1 shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] transition">
           Facile
         </p>
-        <p className="bg-purple-heart-500 px-10 rounded-lg hover:bg-purple-heart-800 py-1 shadow-lg">
+        <p className="bg-purple-heart-500 px-24 text-center rounded-lg hover:bg-purple-heart-800 py-1 shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] transition">
           Moyen
         </p>
-        <p className="bg-purple-heart-500 px-10 rounded-lg hover:bg-purple-heart-800 py-1 shadow-lg">
+        <p className="bg-purple-heart-500 px-24 text-center rounded-lg hover:bg-purple-heart-800 py-1 shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] transition">
           Difficile
         </p>
       </div>
