bee006f6f1ebf3ddaabe5e491e791bab7642965c
Baris G <barisg@pm.me>
2023-10-26 18:53:24 +0200

    Ajout Footer pour le Routage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import OngletElixir from "./components/OngletElixir";
 import Accueil from "./components/Accueil";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./App.scss";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Accueil />} />
         <Route path="/elixir" element={<OngletElixir />} />
       </Routes>
+      <Footer />
     </>
   );
 }
