0b90fa28f22e96527fd3cb9d007a1bb8a1d400ed
Baris G <barisg@pm.me>
2023-10-26 17:08:03 +0200

    premier routage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import OngletElixir from "./components/OngletElixir";
 import Accueil from "./components/Accueil";
+import NavBar from "./components/NavBar";
 import "./App.scss";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Accueil />} />
-      <Route path="/elixir" element={<OngletElixir />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/elixir" element={<OngletElixir />} />
+      </Routes>
+    </>
   );
 }
 export default App;
