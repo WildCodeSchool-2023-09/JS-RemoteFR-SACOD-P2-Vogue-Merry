a96d872ec8c325bb76c720aaa353714229ad0b8f
Baris G <barisg@pm.me>
2023-10-26 10:08:39 +0200

    First Commit pour le routage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import "./App.scss";
 import OngletElixir from "./components/OngletElixir";
+import logo from "./assets/logo.svg";
+import "./App.scss";
 
 function App() {
-  return <OngletElixir />;
+  return (
+    <>
+      <nav>
+        <img src={logo} alt="logo" className="h-20" />
+        <ul>
+          <li>Test1</li>
+          <li>Test2</li>
+          <li>Test3</li>
+        </ul>
+      </nav>
+      <OngletElixir />
+    </>
+  );
 }
 
 export default App;
