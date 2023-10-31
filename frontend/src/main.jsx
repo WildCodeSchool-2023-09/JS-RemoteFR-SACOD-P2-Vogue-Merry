9619be6607bf4846c89589efa2352dfe807393da
Baris G <barisg@pm.me>
2023-10-31 16:48:37 +0100

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
 import OngletElixir from "./components/OngletElixir";
@@ -8,6 +9,7 @@ import Accueil from "./components/Accueil";
 import OngletLevel from "./components/OngletLevel";
 import Rules from "./components/Rules";
 import Jeu from "./components/Jeu";
+import Leaderboard from "./pages/Leaderboard";
 
 const router = createBrowserRouter([
   {
@@ -34,6 +36,10 @@ const router = createBrowserRouter([
       {
         path: "rules",
         element: <Rules />,
+      },
+      {
+        path: "leaderboard",
+        element: <Leaderboard />,
       },
     ],
   },
---
+++ frontend/src/pages/Leaderboard.jsx
@@ -0,0 +1,5 @@
+function Leaderboard() {
+  return <div className="max-w-7xl bg-white">Test</div>;
+}
+
+export default Leaderboard;
