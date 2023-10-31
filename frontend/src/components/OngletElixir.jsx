21ede39846ba5944e76e026e47c47d975442d875
baris.g <67648495+BarisRz@users.noreply.github.com>
2023-10-31 11:46:58 +0100

    Update OngletElixir.jsx

--- frontend/src/components/OngletElixir.jsx
+++ frontend/src/components/OngletElixir.jsx
@@ -58,7 +58,7 @@ function OngletElixir() {
       <div className="filtre-elixir flex justify-center gap-10 max-sm:gap-2 text-white p-6 font-bold max-sm:flex-col mb-10">
         <button
           type="button"
-          onClick={() => handleDifficulty(undefined)}
+          onClick={() => handleDifficulty(null)}
           className={boutonStyle}
         >
           All
