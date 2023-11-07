0638cc5704bfcb2d2ccb95e9caf286690a188f44
MORGANE DEBARGE <morganedebarge@msn.com>
2023-11-07 11:11:22 +0100

    ok fonctionnel

--- frontend/src/components/Jeu.jsx
+++ frontend/src/components/Jeu.jsx
@@ -31,7 +31,7 @@ function Jeu() {
     pvs = [1, 2];
   }
   const [pV, setPV] = useState(pvs);
-
+  console.info(pV);
   const ingredientsPotion = potions[0]?.attributes.ingredients.split(",");
 
   let wrongIngredients = [];
@@ -81,79 +81,87 @@ function Jeu() {
             Find {ingredientsPotion?.length} ingredients
           </p>
         </div>
-        <div
-          className="flex justify-between w-5/6  p-3 max-sm:w-full max-sm:h-48 items-center
+        {pV.length !== 0 ? (
+          <>
+            <div
+              className="flex justify-between w-5/6  p-3 max-sm:w-full max-sm:h-48 items-center
   
         "
-        >
-          <div
-            className="bg-[url('./assets/parch.png')] bg-no-repeat h-72 w-80  flex flex-col  items-center justify-center
+            >
+              <div
+                className="bg-[url('./assets/parch.png')] bg-no-repeat h-72 w-80  flex flex-col  items-center justify-center
           max-sm:w-2/6 max-sm:h-28 max-sm:bg-cover max-sm:p-0  max-sm:gap-0 "
-          >
-            <img
-              src={potions[0]?.attributes.image}
-              alt="potion"
-              className="w-14 h-12  max-sm:w-6 max-sm:h-6 "
-            />
-            <span
-              className="w-26 text-white text-center font-irish flex flex-col
+              >
+                <img
+                  src={potions[0]?.attributes.image}
+                  alt="potion"
+                  className="w-14 h-12  max-sm:w-6 max-sm:h-6 "
+                />
+                <span
+                  className="w-26 text-white text-center font-irish flex flex-col
             max-sm:w-18 max-sm:text-xs 
             "
-            >
-              <p>{potions[0]?.attributes.name}</p>
-              <p>{potions[0]?.attributes.characteristics}</p>
-              <p>{potions[0]?.attributes.effect} </p>
-            </span>
-          </div>
-          <span className={imgIngredientClass}>
-            <img src={imgIngredient} alt={imgIngredient} />
-          </span>
-          <div
-            className="text-white h-48 w-72 p-10 rounded-2xl bg-purple-heart-500 flex flex-col  gap-10 justify-center
+                >
+                  <p>{potions[0]?.attributes.name}</p>
+                  <p>{potions[0]?.attributes.characteristics}</p>
+                  <p>{potions[0]?.attributes.effect} </p>
+                </span>
+              </div>
+              <span className={imgIngredientClass}>
+                <img src={imgIngredient} alt={imgIngredient} />
+              </span>
+              <div
+                className="text-white h-48 w-72 p-10 rounded-2xl bg-purple-heart-500 flex flex-col  gap-10 justify-center
           max-sm:w-28 max-sm:h-16 max-sm:p-0 max-sm:gap-0 align-middle"
-          >
-            <div className="flex flex-row justify-around max-sm:justify-center">
-              <h3 className="self-center font-irish">PV</h3>
-              {pV?.map((pv) => (
-                <div key={pv}>
-                  <img
-                    src={PV}
-                    alt="PV"
-                    className="w-15 h-12 max-sm:w-6 max-sm:h-6  max-sm:text-xs"
+              >
+                <div className="flex flex-row justify-around max-sm:justify-center">
+                  <h3 className="self-center font-irish">PV</h3>
+                  {pV?.map((pv) => (
+                    <div key={pv}>
+                      <img
+                        src={PV}
+                        alt="PV"
+                        className="w-15 h-12 max-sm:w-6 max-sm:h-6  max-sm:text-xs"
+                      />
+                    </div>
+                  ))}
+                </div>
+                <div className="flex flex-row justify-around max-sm:justify-center">
+                  <h3 className="font-irish">Score </h3>
+                  <h4 className="font-irish">{score}/200</h4>
+                </div>
+              </div>
+            </div>
+            <span className="w-full h-3/6 ">
+              <Chaudron bg={bg} />
+            </span>
+            <div className=" flex flex-wrap justify-center  ">
+              {allIngredients?.map((ingredient) => (
+                <div
+                  key={ingredient}
+                  className="flex  bg-purple-heart-500 w-56 h-10 rounded m-2 max-sm:w-42"
+                >
+                  <Ingredients
+                    ingredient={ingredient}
+                    ingredientsPotion={ingredientsPotion}
+                    setImgIngredient={setImgIngredient}
+                    setBg={setBg}
+                    setImgIngredientClass={setImgIngredientClass}
+                    score={score}
+                    setScore={setScore}
+                    gainScore={gainScore}
+                    setPV={setPV}
+                    pV={pV}
                   />
                 </div>
               ))}
             </div>
-            <div className="flex flex-row justify-around max-sm:justify-center">
-              <h3 className="font-irish">Score </h3>
-              <h4 className="font-irish">{score}/200</h4>
-            </div>
-          </div>
-        </div>
-        <span className="w-full h-3/6 ">
-          <Chaudron bg={bg} />
-        </span>
-        <div className=" flex flex-wrap justify-center  ">
-          {allIngredients?.map((ingredient) => (
-            <div
-              key={ingredient}
-              className="flex  bg-purple-heart-500 w-56 h-10 rounded m-2 max-sm:w-42"
-            >
-              <Ingredients
-                ingredient={ingredient}
-                ingredientsPotion={ingredientsPotion}
-                setImgIngredient={setImgIngredient}
-                setBg={setBg}
-                setImgIngredientClass={setImgIngredientClass}
-                score={score}
-                setScore={setScore}
-                gainScore={gainScore}
-                setPV={setPV}
-                pV={pV}
-              />
-            </div>
-          ))}
-        </div>
+          </>
+        ) : (
+          <p className="text-white font-irish w-5/6 h-3/6 flex justify-center items-center text-7xl">
+            Game Over
+          </p>
+        )}
       </div>
     </div>
   );
