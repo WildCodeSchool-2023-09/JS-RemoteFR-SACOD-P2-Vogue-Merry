f4290e14ea142c8ae464d9ed71fbc208640f70f1
Elie Rakoto <deicide21@hotmail.fr>
2023-11-10 10:27:47 +0100

    boutton try again

--- frontend/src/components/Victory.jsx
+++ frontend/src/components/Victory.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 function Victory({ result }) {
   if (result) {
@@ -9,11 +10,31 @@ function Victory({ result }) {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }}
       >
-        Victory!
+        <p>Victory!</p>
+        <Link to="/difficulty">
+          <button
+            type="button"
+            className="flex bg-purple-heart-500 rounded-2xl text-white w-full h-full justify-center items-center z-10 "
+          >
+            Try again
+          </button>
+        </Link>
       </motion.p>
     );
   }
-  return <p>Game Over!</p>;
+  return (
+    <div>
+      <p>Game Over!</p>
+      <Link to="/difficulty">
+        <button
+          type="button"
+          className="flex bg-purple-heart-500 rounded-2xl text-white w-full h-full justify-center items-center z-10 hover"
+        >
+          Try again
+        </button>
+      </Link>
+    </div>
+  );
 }
 
 Victory.propTypes = {
